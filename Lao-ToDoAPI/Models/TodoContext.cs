10f7daf2eddfdf2974d7bcf84927b700ba2f149f
Jeanina Lao <lao13001@byui.edu>
2018-11-27 18:40:46 -0700

    Final commit

---
+++ Lao-ToDoAPI/Models/TodoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lao_ToDoAPI.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
--- Lao-ToDoAPI/Startup.cs
+++ Lao-ToDoAPI/Startup.cs
@@ -10,11 +10,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using Lao_ToDoAPI.Models;
 
 namespace Lao_ToDoAPI
 {
     public class Startup
     {
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -25,6 +28,8 @@ namespace Lao_ToDoAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
